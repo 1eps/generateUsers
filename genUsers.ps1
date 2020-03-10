f24825440239df9ea233fe0a170537c7fb3f0d06
1eps <59583022+1eps@users.noreply.github.com>
2020-03-11 01:36:26 +0300

    Update genUsers.ps1
    Написал метод выдающий рандомное имя из словаря

--- genUsers.ps1
+++ genUsers.ps1
@@ -1,5 +1,25 @@
-## Generate random users for  Active Directory
+## Generate random male usernames for Active Directory
 
-$Array = @(Get-Content .\Desktop\dev\ps\mNames.txt) ## Должны быть по строкам имена, отчества, фамилии
-$Array
+$male = randomNames(@(Get-Content C:\Users\1eps\Desktop\dev\generateUsers-master\generateUsers-master\mNames.txt))
+$male
+$feMale = randomNames(@(Get-Content C:\Users\1eps\Desktop\dev\generateUsers-master\generateUsers-master\fNames.txt))
+$feMale
 
+function randomNames($Array) { ## Generate random usernames for Active Directory
+$Names = @($Array[0].split(' ')) ## Выделяю первую строку - массив с именами
+$NamesCount = $Names.Count - 1 ## Переменная для конечного числа будущего рэндома, чтобы не править каждый раз при расширении словаря
+$nRand = Get-Random (0..$NamesCount)## Генерирую рэндом коэффициент
+$Name = $Names[$nRand] ##Переменная имени в зависимости от выбранного рэндомного числа
+
+$FathersNames = @($Array[1].split(' ')) ## Выделяю вторую строку - массив с отчествами
+$FathersNamesCount = $FathersNames.Count - 1 ## Переменная для конечного числа будущего рэндома, чтобы не править каждый раз при расширении словаря
+$fRand = Get-Random (0..$FathersNamesCount)## Генерирую рэндом коэффициент
+$FathersName = $FathersNames[$fRand] ##Переменная отчества в зависимости от выбранного рэндомного числа
+
+$LastNames = @($Array[2].split(' ')) ## Выделяю третью строку - массив с фамилиями
+$LastNamesCount = $LastNames.Count - 1 ## Переменная для конечного числа будущего рэндома, чтобы не править каждый раз при расширении словаря
+$lRand = Get-Random (0..$LastNamesCount)## Генерирую рэндом коэффициент
+$LastName = $LastNames[$lRand] ##Переменная отчества в зависимости от выбранного рэндомного числа
+
+return $fio = $LastName + " " + $Name + " " + $FathersName ##создаю строку ФИО
+}
